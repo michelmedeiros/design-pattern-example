ac653837b41c38dd4f6e9f9de36d0e32700ea501
Michel Medeiros <michel.tds@gmail.com>
2022-05-17 19:51:18 -0300

    First commit

---
+++ src/test/java/com/br/example/designpattern/DesignpatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.example.designpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
